16e89289ec72ca39506179d646c96d5b0ce99448
Benjamin Aballay <bennya82@gmail.com>
2018-05-05 20:03:02 +0300

    recycleview + design layouts

--- app/src/main/java/com/bennyab/tar27042018/ContactsActivity.java
+++ app/src/main/java/com/bennyab/tar27042018/ContactsActivity.java
@@ -2,7 +2,11 @@ package com.bennyab.tar27042018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
 
+import com.bennyab.tar27042018.Adapters.ContactsAdapter;
 import com.bennyab.tar27042018.Data.Contact;
 
 import java.util.ArrayList;
@@ -13,11 +17,31 @@ public class ContactsActivity extends AppCompatActivity {
     private final int NUM_OF_CONTACTS = 10;
     private ArrayList<Contact> contactsData;
 
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ContactsAdapter adapter;
+    private RecyclerView recyclerView;
+    private EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         setTitle(R.string.ContactsTitle);
+        bindView();
+    }
+
+    private void bindView(){
+        recyclerView = findViewById(R.id.act_contacts_recycleViewList);
+        editText = findViewById(R.id.act_contacts_et_search);
+
+        generateStubData();
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        adapter = new ContactsAdapter(contactsData);
+        recyclerView.setAdapter(adapter);
+
+
     }
 
     private void generateStubData(){
